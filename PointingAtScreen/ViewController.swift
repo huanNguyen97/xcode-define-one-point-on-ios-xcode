9e29b8ba97357c77eb073ee6f2a78dc66559c1a7
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-24 09:11:39 +0700

    OK2

--- PointingAtScreen/ViewController.swift
+++ PointingAtScreen/ViewController.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let startPoint = StartPoint()
+    let startPoint = Point(x1: 120, y1: 120, x2: 300, y2: 300)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         startPoint.translatesAutoresizingMaskIntoConstraints = false
-        startPoint.backgroundColor = .white
+        startPoint.backgroundColor = .blue
         
         view.addSubview(startPoint)
         
@@ -32,10 +32,24 @@ class ViewController: UIViewController {
     
 }
 
-class StartPoint: UIView {
+class Point: UIView {
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    // Declare w - h for pointing
+    var x1: Double
+    var y1: Double
+    var x2: Double
+    var y2: Double
+    // End declaring
+    
+    init(x1: Double, y1: Double, x2: Double, y2: Double) {
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        
+        // CAUTION: Not clearly at this line but it's not a bug
+        super.init(frame: CGRect(x: self.x1 + self.x2, y: self.y1 + self.y2, width: 2, height: 2))
+        // End caution
     }
     
     required init?(coder: NSCoder) {
@@ -46,14 +60,25 @@ class StartPoint: UIView {
         
         guard let context = UIGraphicsGetCurrentContext() else { return }
         
-        let rectangle1 = CGRect(x: 100, y: 90, width: 2, height: 2)
+        // Point 1 - startPoint
+        let rectangle1 = CGRect(x: self.x1, y: self.y1, width: 2, height: 2)
         
         context.setFillColor(UIColor.systemRed.cgColor)
         context.setStrokeColor(UIColor.systemRed.cgColor)
         context.setLineWidth(20)
         context.addRect(rectangle1)
         context.drawPath(using: .fillStroke)
-        context.fill(rectangle1)
+        // End point
+        
+        // Point 2 - endPoint
+        let rectangle2 = CGRect(x: self.x2, y: self.y2, width: 2, height: 2)
+
+        context.setFillColor(UIColor.systemGreen.cgColor)
+        context.setStrokeColor(UIColor.systemGreen.cgColor)
+        context.setLineWidth(20)
+        context.addRect(rectangle2)
+        context.drawPath(using: .fillStroke)
+        // End point
     }
 }
 
